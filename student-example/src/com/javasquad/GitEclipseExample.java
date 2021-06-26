0c2c03aa004efbd312d6fb2c5027992b59d8473b
anilkpo <ponnamanikumar9@gmail.com>
2021-06-26 12:51:14 +0530

    updating the repo with student examples

---
+++ student-example/src/com/javasquad/GitEclipseExample.java
@@ -0,0 +1,11 @@
+package com.javasquad;
+
+public class GitEclipseExample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello team welcome to Eclipse and git");
+
+	}
+
+}
